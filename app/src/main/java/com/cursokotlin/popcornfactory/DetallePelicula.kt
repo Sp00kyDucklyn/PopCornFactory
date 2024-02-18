cc2ca595ceb8c8039f8ea3b327aa54e59c0aaa87
Sp00kyDucklyn <hoshi.saga.dokuringo@gmail.com>
2024-02-18 16:21:26 -0700

    Design Part

---
+++ app/src/main/java/com/cursokotlin/popcornfactory/DetallePelicula.kt
@@ -0,0 +1,11 @@
+package com.cursokotlin.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetallePelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pelicula)
+    }
+}
